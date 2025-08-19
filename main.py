53fcc250a3bf3541c63bf49a6580471dfe85acf3
MinJun2 <kaf829@naver.com>
2025-08-19 10:11:19 +0900

    production_chart main.py 추가

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ from app.controllers.process_line_controller import router as process_router # �
 from app.controllers.production_grid_controller import router as production_grid_router
 from app.controllers import downtime_grid_controller # Downtime(비가동) 컨트롤러 모듈
 from app.controllers.user_grid_controller import router as user_grid_router
-
+from app.controllers.production_chart_controller import router as production_chart_router
 # ============================================================================
 # FastAPI 애플리케이션 생성 및 설정
 # ============================================================================
@@ -78,6 +78,8 @@ app.include_router(process_router, prefix="/smartFactory")
 app.include_router(production_grid_router, prefix="/smartFactory")
 app.include_router(downtime_grid_controller.router, prefix="/smartFactory")
 app.include_router(user_grid_router, prefix="/smartFactory")  # ✅ /smartFactory/user_grid/...
+app.include_router(production_chart_router, prefix="/smartFactory")
+
 
 # ✅ 인증 도메인 (/auth/login, /auth/me 등)
 app.include_router(auth_router)
